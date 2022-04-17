8c3dbf8e3cb7bd9aad334a8f05197269aa9ba4f1
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-04-17 15:40:27 +0100

    Create demo.sh

---
+++ api/demo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. common.sh
+
+###
+
+token=$(get apitoken)
+host=10.1.0.78:8123
+
+curl -X POST -H 'Authorization: Bearer ${token}' -H 'Content-Type: application/json' \
+http://${host}/api/states/sensor.hot_water
+
+
+#EOF
