d990f21c07b64c47f5ea837d438cba52e5680474
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-04-17 15:45:20 +0100

    Update demo.sh

--- api/demo.sh
+++ api/demo.sh
@@ -7,7 +7,7 @@
 token=$(get apitoken)
 host=10.1.0.78:8123
 
-curl -X POST -H 'Authorization: Bearer '${token} -H 'Content-Type: application/json' \
+curl -X GET -H 'Authorization: Bearer '${token} \
 http://${host}/api/states/sensor.hot_water
 
 
