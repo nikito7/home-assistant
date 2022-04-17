549b925aba7c91cd8cbe7b0677012669928a784e
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-04-17 15:48:33 +0100

    Update demo.sh

--- api/demo.sh
+++ api/demo.sh
@@ -8,7 +8,8 @@ token=$(get apitoken)
 host=10.1.0.78:8123
 
 curl -X GET -H 'Authorization: Bearer '${token} \
-http://${host}/api/states/sensor.hot_water
+http://${host}/api/states/sensor.hot_water \
+awk -F \" '{ print $7 }'
 
 
 #EOF
