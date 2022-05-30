4676743b3adc0efe68a1cf812d5e6c18f5d7475d
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-05-30 17:06:37 +0100

    Update cpu2mqtt.bat

--- windows/cpu2mqtt.bat
+++ windows/cpu2mqtt.bat
@@ -3,14 +3,16 @@
 set server=
 set user=
 set pass=
+set topic=
 
 rem ### code ###
 
-set usage=0
+set usage=-1
 
 for /f "skip=1" %%p in ('wmic cpu get loadpercentage ^| findstr /r /v "^$"') do (
 set usage=%%p
-echo debug: %usage%
 )
+
+echo debug: %usage% %%
 
 rem ### eof ###
