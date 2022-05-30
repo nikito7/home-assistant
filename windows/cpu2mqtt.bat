bc4213f9b688a088e4ebcda490c518ea601a7b8e
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-05-30 17:24:31 +0100

    Update cpu2mqtt.bat

--- windows/cpu2mqtt.bat
+++ windows/cpu2mqtt.bat
@@ -3,8 +3,8 @@
 set server=10.1.0.78
 set user=mqtt2021
 set pass=
-set topic=windows/pc/cpu
-set app="./mqtt/mosquitto_pub.exe -h %server% -u %user% -P %pass%"
+set topic=/windows/pc/cpu_usage
+set app=.\mqtt\mosquitto_pub.exe -h %server% -u %user% -P %pass%
 
 rem ### code ###
 
@@ -17,5 +17,7 @@ set usage=%%p
 echo debug: %usage% %%
 
 %app% -t %topic% -m %usage%
+
+timeout 5
 
 rem ### eof ###
