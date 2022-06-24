411362d67158d83f802e654e0f5387db449383cb
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-06-24 12:39:18 +0100

    Update demo-switch.sh

--- api/demo-switch.sh
+++ api/demo-switch.sh
@@ -7,9 +7,9 @@
 token=$(get apitoken)
 host=10.1.0.78:8123
 
-curl -s -X GET -H 'Authorization: Bearer '${token} \
-http://${host}/api/states/sensor.hot_water \
-| awk -F "\"" '{ print $8 }'
-
+curl -X POST -H 'Authorization: Bearer '${apitoken} \
+-H "Content-Type: application/json" \
+-d '{"entity_id": "switch.athom_p1"}' \
+http://${host}/api/services/switch/turn_on
 
 #EOF
