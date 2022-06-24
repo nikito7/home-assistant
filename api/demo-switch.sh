99c83c347eb78babad7a2b8c5d2b8a34e9032f01
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-06-24 12:51:01 +0100

    Update demo-switch.sh

--- api/demo-switch.sh
+++ api/demo-switch.sh
@@ -4,7 +4,7 @@ secrets=./secrets.yaml
 token=$(cat $secrets | grep apitoken | awk -F ": " '{ print $2 }')
 host=10.1.0.78:8123
 
-curl -X POST -H 'Authorization: Bearer '${apitoken} \
+curl -X POST -H 'Authorization: Bearer '${token} \
 -H "Content-Type: application/json" \
 -d '{"entity_id": "switch.athom_p1"}' \
 http://${host}/api/services/switch/turn_on
